54a75226ce5bf7f78afcd87aef7a415b412cebf1
Hung Viet Nguyen <henry@gotitapp.co>
2020-06-11 23:34:39 +0700

    chore: update config

--- site-config.js
+++ site-config.js
@@ -4,13 +4,13 @@ module.exports = {
   siteTitle: "Hieu's Sketches",
   siteTitleShort: "Hieu's Sketches",
   siteDescription: "Hieu's Sketches Gallery",
-  siteUrl: 'https://gu.fabianschultz.com',
+  siteUrl: 'https://sicula.netlify.app',
   themeColor: '#000',
   backgroundColor: '#fff',
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/images/icon.png'),
   social: {
-    twitter: 'gatsbyjs',
-    fbAppId: '966242223397117',
+    twitter: '',
+    fbAppId: '',
   },
 };
